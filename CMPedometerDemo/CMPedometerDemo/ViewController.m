bd9c5852c2cd851cff385b771c0c184d428e8fc0
EmyWong <woxiangni321@qq.com>
2017-08-29 15:57:04 +0800

    计步器

--- CMPedometerDemo/CMPedometerDemo/ViewController.m
+++ CMPedometerDemo/CMPedometerDemo/ViewController.m
@@ -18,30 +18,42 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    //如果计步服务可用
+    //创建CMPedometer对象
+    self.pedoMeter = [[CMPedometer alloc] init];
+}
+- (IBAction)startCountStep:(id)sender {
+    //判断是否可以支持获取步数
     if ([CMPedometer isStepCountingAvailable]) {
-        //创建CMPedometer对象
-        self.pedoMeter = [[CMPedometer alloc] init];
-        //开始收集计步信息
+        //判断是否可以支持返回步行的距离
+        //    if ([CMPedometer isDistanceAvailable]) {
+        //判断是否可以支持爬楼梯的检测
+        //    if ([CMPedometer isFloorCountingAvailable]) {
+        
+        //当你的步数有更新的时候，会触发这个方法，返回从某一时刻开始到现在所有的信息统计CMPedometerData。
+        //其中CMPedometerData包含步数等信息，在下面使用的时候介绍。
+        //值得一提的是这个方法不会实时返回结果，每次刷新数据大概一分钟左右。
         [self.pedoMeter startPedometerUpdatesFromDate:[NSDate date] withHandler:^(CMPedometerData * _Nullable pedometerData, NSError * _Nullable error) {
-            NSLog(@"%@",pedometerData.numberOfSteps);
-            self.stepLabel.text = [NSString stringWithFormat:@"我已经走了%@步",pedometerData.numberOfSteps];
+            self.stepLabel.text = [NSString stringWithFormat:@"我已经走了%@步，距离%@米",pedometerData.numberOfSteps,pedometerData.distance];
         }];
-        //限定时间内获取计步数据
-//        [self.pedoMeter queryPedometerDataFromDate:[NSDate date] toDate:[NSDate dateWithTimeIntervalSinceNow:10] withHandler:^(CMPedometerData * _Nullable pedometerData, NSError * _Nullable error) {
-//            NSLog(@"%@",pedometerData.numberOfSteps);
-//        }];
-    
+        
     } else {
         NSLog(@"设备不可用");
     }
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)stopCountStep:(UIButton *)sender {
+    //停止收集计步信息
+    [self.pedoMeter stopPedometerUpdates];
 }
+- (IBAction)lastWeek:(id)sender {
+    //这个方法用来查询近7天内的任意时间段的CMPedometerData信息。
+    //这个方法是当你请求的时候才会触发
+    
+    //我查询从今天0点到现在我走了多少步
+    NSDate *startOfToday = [[NSCalendar currentCalendar] startOfDayForDate:[NSDate date]];
+    [self.pedoMeter queryPedometerDataFromDate:startOfToday toDate:[NSDate dateWithTimeIntervalSinceNow:0] withHandler:^(CMPedometerData * _Nullable pedometerData, NSError * _Nullable error) {
+        self.stepLabel.text = [NSString stringWithFormat:@"我已经走了%@步，距离%@米",pedometerData.numberOfSteps,pedometerData.distance];
+    }];
 
-
+}
 @end
